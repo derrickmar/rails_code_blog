352293b51cecf1b84feeedd7fb640ab516dd8dda
Derrick Mar <derrickmar1215@berkeley.edu>
2014-11-27 02:08:47 -0800

    finish index functionalaity direction hover aware

--- app/assets/javascripts/rails_blog/under_the_hood_posts.js
+++ app/assets/javascripts/rails_blog/under_the_hood_posts.js
@@ -1,96 +1,154 @@
 (function($, window, document) {
-  $("document").ready(function() {
-      tips.onReady($('.question'));
-      var sidebar = new Sidebar($('.sidebar'))
-      sidebar.onReady();
+    $(window).load(function() {
+        console.log('running?');
+        tips.onReady($('.question'));
+        var sidebar = new Sidebar($('.sidebar'))
+        sidebar.onReady();
+        causesHover.onReady();
+    });
+
+    var causesHover = {
+        onReady: function() {
+            causesHover.fadeInOutTitle($('.posts-holder'))
+            .hoverDirectionAware($('.da-thumbs-2 > li'))
+        },
+        fadeInOutTitle: function(el) {
+                // hover functionality but using event delegation strategy
+                // event handler only attached to causes-holder
+                // event bubbling means you can add extra li's dynamically and funciton will still work
+                el.on('mouseenter', 'li', function() {
+                    // TODO - setTimeout to 100 as in hoverDelay
+                    $(this).find('.all-causes-title').fadeOut(250);
+                    console.log('in hover');
+                }).on('mouseleave', 'li', function() {
+                    $(this).find('.all-causes-title').fadeIn(250); // repetition!!
+                });
+                return causesHover;
+            },
+            hoverDirectionAware: function(el) {
+                el.each(function() {
+                    $(this).hoverdir({
+                        speed: 300,
+                        hoverDelay: 100
+                    });
+                });
+                return causesHover;
+            }
+        }
+
+        var tips = {
+            onReady: function(box) {
+                box.on("click", function() {
+                    console.log('clicked question!');
+                    $(this).siblings('.box-description').toggleClass('hidden');
+                    var arrow = $(this).find('.expanding-arrow');
+                    console.log(arrow);
+                    if (arrow.hasClass('glyphicon-chevron-down')) {
+                        arrow.removeClass('glyphicon-chevron-down');
+                        arrow.addClass('glyphicon-chevron-up');
+                    } else {
+                        arrow.removeClass('glyphicon-chevron-up');
+                        arrow.addClass('glyphicon-chevron-down');
+                    }
+                });
+            }
+        }
+
+        function Sidebar(el) {
+            var sidebar = el;
+            var obj = this;
+            var distances = {};
+            var distances_keys;
+            var header_height = 50;
+            $window = $(window);
+            var slideDownCurr;
+
+            this.onReady = function() {
+
+                var sections = $('.content-box').children('h1, h2, h4');
+                var sidebar_sections = sidebar.find("a");
+
+                for (i = 0; i < sections.length; i++) {
+                    var distance = $(sections[i]).offset().top;
+                    console.log("distance", distance);
+                    distances[distance] = $(sections[i]);
+                }
+
+                console.log(distances);
+                distances_keys = Object.keys(distances).sort(function(a, b){return a - b});
+            // sort distances_keys?
+            console.log(distances_keys);
+
+            $(sidebar_sections).on("click", function() {
+                obj.changeSelected($(this));
+            });
 
 
-      var tips = {
-          onReady: function(box) {
-              box.on("click", function() {
-                  console.log('clicked question!');
-                  $(this).siblings('.box-description').toggleClass('hidden');
-                  var arrow = $(this).find('.expanding-arrow');
-                  console.log(arrow);
-                  if (arrow.hasClass('glyphicon-chevron-down')) {
-                      arrow.removeClass('glyphicon-chevron-down');
-                      arrow.addClass('glyphicon-chevron-up');
-                  } else {
-                      arrow.removeClass('glyphicon-chevron-up');
-                      arrow.addClass('glyphicon-chevron-down');
-                  }
-              });
-          }
-      }
+            $window.scroll(function() {
+                var scrollTop = $window.scrollTop();
+                var pos = obj.returnPosition(scrollTop)
+                    // console.log("position: ", pos);
+                    var name = "#" + $(distances[pos][0]).children("a").attr("name");
+                // console.log($('a[href=' + name + ']'));
+                // if ($('a[href=' + name + ']'))
+                var sidebar_tab = $('a[href=' + name + ']');
+                if (!sidebar_tab.hasClass('selected')) {
+                    console.log('scrollTop', scrollTop);
+                    obj.changeSelected($('a[href=' + name + ']'));
+                }
+            })
+        };
 
-      function Sidebar(el) {
-          var sidebar = el;
-          var obj = this;
-          var distances = {};
-          var distances_keys;
-          var header_height = 50;
-          var initializeOnce = true;
+        /* Changes the selected sidebar tab based on the positiion of blog
+        headers. Also slides down nested-two if it is in that section */
+        this.changeSelected = function(el) {
+            if (slideDownCurr != undefined) {
+                obj.checkSlideUp(el);
+            }
+            var parent = el.parent();
+            var siblHasClassNestedTwo = el.siblings('.nested-two');
+            if (parent.hasClass('nested-two') && parent.css('display') == "none") {
+                console.log('sliding down nested-two first');
+                console.log("parent", parent);
+                parent.slideDown();
+                slideDownCurr = parent.parent();
+                slideDownCurr.addClass('nested-two-focused');
+                // might error out if siblNestedTwo has not elements
+            } else if (siblHasClassNestedTwo.length == 1 && $(siblHasClassNestedTwo[0]).css('display') == "none") {
+                var nestedTwo = $(siblHasClassNestedTwo[0])
+                console.log('sliding down nested-two');
+                nestedTwo.slideDown();
+                slideDownCurr = nestedTwo.parent(); // .nested-two-container 
+                slideDownCurr.addClass('nested-two-focused');
+            }
+            // console.log('change selected!');
+            sidebar.find('.selected').removeClass('selected');
+            el.addClass('selected');
+        };
 
-          $window = $(window);
+        /* Checks if the nested-two div must slide up if you are in a different
+        section */
+        this.checkSlideUp = function(el) {
+            console.log(el[0]);
+            console.log(!$.contains(slideDownCurr[0], el[0]));            
+            if (!$.contains(slideDownCurr[0], el[0])) {
+                console.log('should slideUp!!');
+                slideDownCurr.removeClass('nested-two-focused');
+                slideDownCurr.find('.nested-two').slideUp();
+            }
+        };
 
-          this.onReady = function() {
-
-              var sections = $('.content-box').children('h1, h2, h4');
-              var sidebar_sections = sidebar.find("a");
-
-              $(sidebar_sections).on("click", function() {
-                  obj.changeSelected($(this));
-              });
-
-
-              $window.scroll(function() {
-                  if (initializeOnce) {
-                      obj.initializeDistances(sections);
-                  }
-                  var scrollTop = $window.scrollTop();
-                  var pos = obj.returnPosition(scrollTop)
-                      // console.log("position: ", pos);
-                  var name = "#" + $(distances[pos][0]).children("a").attr("name");
-                  // console.log($('a[href=' + name + ']'));
-                  // if ($('a[href=' + name + ']'))
-                  var sidebar_tab = $('a[href=' + name + ']');
-                  if (!sidebar_tab.hasClass('selected')) {
-                      console.log('scrollTop', scrollTop);
-                      obj.changeSelected($('a[href=' + name + ']'));
-                  }
-              })
-          };
-
-          this.initializeDistances = function(sections) {
-              initializeOnce = false;
-              for (i = 0; i < sections.length; i++) {
-                  var distance = $(sections[i]).offset().top;
-                  console.log("distance", distance);
-                  distances[distance] = $(sections[i]);
-              }
-              console.log(distances);
-              distances_keys = Object.keys(distances);
-              console.log(distances_keys);
-          };
-
-          this.changeSelected = function(el) {
-              console.log('change selected!');
-              sidebar.find('.selected').removeClass('selected');
-              el.addClass('selected');
-          };
-
-          this.returnPosition = function(scrollTop) {
-              for (i = 0; i < distances_keys.length; i++) {
-                  if (distances_keys[i] > scrollTop + header_height + 10) {
-                      var position = Math.max(i - 1, 0)
-                          // console.log('position', position);
-                      return distances_keys[position]
-                  }
-              }
-              return distances_keys[distances_keys.length - 1];
-          }
-      }
-  });
+        this.returnPosition = function(scrollTop) {
+            for (i = 0; i < distances_keys.length; i++) {
+                if (distances_keys[i] > scrollTop + header_height + 15) {
+                    var position = Math.max(i - 1, 0)
+                        // console.log('position', position);
+                        return distances_keys[position]
+                    }
+                }
+                return distances_keys[distances_keys.length - 1];
+            }
+        }
 
 }(window.jQuery, window, document)); // The global jQuery object is passed as a parameter
 
--- app/controllers/rails_blog/under_the_hood_posts_controller.rb
+++ app/controllers/rails_blog/under_the_hood_posts_controller.rb
@@ -3,10 +3,11 @@ require_dependency "rails_blog/application_controller"
 module RailsBlog
   class UnderTheHoodPostsController < ApplicationController
   	def index
+      @sidebar_partial = 'rails_blog/sidebar/sidebar_index'
   	end
 
-    def deploying_to_heroku
-    end
+    # def deploying_to_heroku
+    # end
 
     def quick_guide_heroku
       @sidebar_partial = 'rails_blog/sidebar/sidebar_quick_guide_heroku'
