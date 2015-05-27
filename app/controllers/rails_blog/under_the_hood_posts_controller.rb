db98a2902ab5f5fc82dfa92e86538fc9cd41ab08
Derrick Mar <derrickmar1215@berkeley.edu>
2015-05-28 09:05:55 +1000

    Dry controller code by adding constant for namespace for sidebar partials

--- app/controllers/rails_blog/under_the_hood_posts_controller.rb
+++ app/controllers/rails_blog/under_the_hood_posts_controller.rb
@@ -2,30 +2,42 @@ require_dependency "rails_blog/application_controller"
 
 module RailsBlog
   class UnderTheHoodPostsController < ApplicationController
+    before_action :append_namespace
+
   	def index
       @sidebar_partial = 'rails_blog/sidebar/sidebar_index'
       @hover_elem_partial_path = 'rails_blog/under_the_hood_posts/partials/grid_hover_elem'
   	end
 
     def ar_relation_lazy_evaluation
-      @sidebar_partial = 'rails_blog/sidebar/sidebar_ar_relation_lazy_evaluation.html.erb'
+      @sidebar_partial += 'sidebar_ar_relation_lazy_evaluation.html.erb'
     end
 
+    def ar_reload
+      @sidebar_partial += 'sidebar_ar_reload.html.erb'
+    end
 
     def ngrok
-      @sidebar_partial = 'rails_blog/sidebar/sidebar_ngrok'
+      @sidebar_partial += 'sidebar_ngrok'
     end
 
     def quick_guide_heroku
-      @sidebar_partial = 'rails_blog/sidebar/sidebar_quick_guide_heroku'
+      @sidebar_partial += 'sidebar_quick_guide_heroku'
     end
 
     def ajax
-      @sidebar_partial = 'rails_blog/sidebar/sidebar_ajax'
+      @sidebar_partial += 'sidebar_ajax'
     end
 
     def angular_performance
-      @sidebar_partial = 'rails_blog/sidebar/sidebar_angular_performance'
+      @sidebar_partial += 'sidebar_angular_performance'
+    end
+
+    private
+    SIDEBAR_NAMESPACE = "rails_blog/sidebar/"
+
+    def append_namespace
+      @sidebar_partial = SIDEBAR_NAMESPACE
     end
   end
 end
